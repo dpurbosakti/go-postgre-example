b82c6b7316bc5e59ccedcb641bcf84f852204bb9
Dwi Atmoko <dwiatmokop@gmail.com>
2023-02-06 10:36:31 +0700

    fix (users) : added cron job

--- app/main.go
+++ app/main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"learn-echo/config"
 	"learn-echo/factory"
+	"learn-echo/features/users/model/domain"
 	"learn-echo/middlewares"
 	"learn-echo/migration"
 	"learn-echo/routes"
 	"os"
 	"os/signal"
 	"runtime/pprof"
+	"text/template"
+
+	"github.com/go-gomail/gomail"
+	"github.com/robfig/cron"
 
 	"github.com/labstack/echo/v4/middleware"
 	log "github.com/sirupsen/logrus"
@@ -26,6 +32,40 @@ func main() {
 	e.Use(middlewares.LogMiddleware)
 	e.Use(middlewares.CorsMiddleware())
 	e.Pre(middleware.RemoveTrailingSlash())
+
+	cron := cron.New()
+	cron.AddFunc("1 * * * *", func() {
+		var users []domain.User
+		result := db.Find(&users)
+		if result.Error != nil {
+			fmt.Println("can't get user data")
+		}
+
+		config := config.Cfg
+		var body bytes.Buffer
+		t, err := template.ParseFiles("../pkg/emailhelper/info.html")
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		m := gomail.NewMessage()
+		m.SetHeader("From", config.EmailConf.Email)
+		m.SetHeader("Subject", "Information")
+
+		for _, v := range users {
+			t.Execute(&body, v)
+			m.SetHeader("To", v.Email)
+			m.SetBody("text/html", body.String())
+			d := gomail.NewDialer(config.EmailConf.Host, config.EmailConf.Port, config.EmailConf.Email, config.EmailConf.Password)
+			err = d.DialAndSend(m)
+			if err != nil {
+				fmt.Println("Error sending email:" + err.Error())
+
+			}
+		}
+		fmt.Println("Email sent.")
+	})
+	cron.Start()
 
 	//add log when c signal sent
 	c := make(chan os.Signal, 1)
--- pkg/emailhelper/emailhelper.go
+++ pkg/emailhelper/emailhelper.go
@@ -8,9 +8,10 @@ import (
 	"learn-echo/features/users/model/domain"
 
 	"github.com/go-gomail/gomail"
+	"gorm.io/gorm"
 )
 
-func SendEmail(user domain.User) error {
+func SendEmailVerCode(user domain.User) error {
 	config := config.Cfg
 	var body bytes.Buffer
 	t, err := template.ParseFiles("../pkg/emailhelper/body.html")
@@ -31,4 +32,34 @@ func SendEmail(user domain.User) error {
 	}
 	fmt.Println("Email sent.")
 	return nil
+}
+
+func SendEmail(db *gorm.DB) {
+	var user []domain.User
+	result := db.Find(&user)
+	if result.Error != nil {
+		fmt.Println("can't get user data")
+	}
+
+	config := config.Cfg
+	var body bytes.Buffer
+	t, err := template.ParseFiles("../pkg/emailhelper/info.html")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	m := gomail.NewMessage()
+	m.SetHeader("From", config.EmailConf.Email)
+	m.SetHeader("Subject", "Information")
+	m.SetBody("text/html", body.String())
+	d := gomail.NewDialer(config.EmailConf.Host, config.EmailConf.Port, config.EmailConf.Email, config.EmailConf.Password)
+	for _, v := range user {
+		m.SetHeader("To", v.Email)
+		t.Execute(&body, v)
+		err := d.DialAndSend(m)
+		if err != nil {
+			fmt.Println("Error sending email:" + err.Error())
+
+		}
+	}
+	fmt.Println("Email sent.")
 }
