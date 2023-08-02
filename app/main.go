c5a36f2538d8d30533003424a6ef55b497bc52cb
Dwi Atmoko <dwiatmokop@gmail.com>
2023-08-02 09:29:28 +0700

    fix: relocate cron

--- app/main.go
+++ app/main.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"learn-echo/config"
+	"learn-echo/cron"
 	"learn-echo/factory"
-	"learn-echo/features/users/models/domain"
 	"learn-echo/migration"
 	"learn-echo/routes"
 	"os"
 	"os/signal"
 	"runtime/pprof"
-	"text/template"
-
-	"github.com/go-gomail/gomail"
-	"github.com/robfig/cron"
 
 	"github.com/sirupsen/logrus"
 )
@@ -28,40 +23,7 @@ func main() {
 	presenter := factory.InitFactory(db)
 	e := routes.New(presenter)
 
-	cron := cron.New()
-	cron.AddFunc("1 * * * *", func() {
-		var users []domain.User
-		result := db.Find(&users)
-		if result.Error != nil {
-			fmt.Println("can't get user data")
-		}
-
-		config := config.Cfg
-		var body bytes.Buffer
-		// t, err := template.ParseFiles("../pkg/emailhelper/info.html")
-		t, err := template.ParseFiles("pkg/emailhelper/info.html")
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-
-		m := gomail.NewMessage()
-		m.SetHeader("From", config.EmailConf.Email)
-		m.SetHeader("Subject", "Information")
-		t.Execute(&body, users)
-		m.SetBody("text/html", body.String())
-
-		for _, v := range users {
-			m.SetHeader("To", v.Email)
-			d := gomail.NewDialer(config.EmailConf.Host, config.EmailConf.Port, config.EmailConf.Email, config.EmailConf.Password)
-			err = d.DialAndSend(m)
-			if err != nil {
-				fmt.Println("Error sending email:" + err.Error())
-
-			}
-		}
-		config.LoggerConf.Info(logrus.WithField("email", "sent"))
-	})
-	cron.Start()
+	cron.InitCron(db)
 
 	//add log when c signal sent
 	c := make(chan os.Signal, 1)
